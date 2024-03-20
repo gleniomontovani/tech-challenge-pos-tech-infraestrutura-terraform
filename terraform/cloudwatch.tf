dbde966e457ebf4d378fed91c960e32f6ac6b761
gleniomontovani <gleniomontovani@gmail.com>
2024-03-19 21:18:07 -0300

    Corrigindo script terraform

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudwatch_log_group" "cloudwatch-log-group" {
-  name              = "eks/${var.projectName}"
+  name              = "ecs/${var.projectName}"
   retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_stream" "cloudwatch-log-stream" {
-  name           = "eks"
+  name           = "ecs"
   log_group_name = aws_cloudwatch_log_group.cloudwatch-log-group.name
 }
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,4 +1,4 @@
-resource "aws_db_subnet_group" "subnet-rds" {
-  name       = "subnet-rds-${var.projectName}"
+resource "aws_db_subnet_group" "subnet-app" {
+  name       = "subnet-app-${var.projectName}"
   subnet_ids = ["${var.subnet01}", "${var.subnet02}", "${var.subnet03}"]
 }
